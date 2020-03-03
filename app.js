18ded095a9368010b6463783457e0d3aad2bba1c
Adel <htwr86@gmail.com>
2020-03-03 16:49:18 +0300

    config and init express server

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const express = require('express');
+const config = require('config');
+
+const PORT = config.get('port') || 5001;
+const app = express();
+
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
