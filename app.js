28a2a9feaaaf2033f21d844b4ac438aa073c1315
Adel <htwr86@gmail.com>
2020-03-03 17:26:52 +0300

    mongo db connected

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
-const PORT = config.get('port') || 5001;
 const app = express();
 
-app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+const PORT = config.get('port') || 5001;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+  } catch (e) {
+    console.log('Server Error', e.message);
+    process.exit(1);
+  }
+}
+
+start();
